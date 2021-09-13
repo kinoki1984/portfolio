e731f6f18217b1dadd277083ad3072f88fc15728
Axel Löwendahl <58688318+kinoki1984@users.noreply.github.com>
2021-09-13 21:02:21 +0200

    Hello world 2!

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          Hello world!
+        </div>
         <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSZ_PvHe6sQXTxUwsTLiqjScZKBdq9JDAwuso9DGKjwWQAqSUN3AxdcHgHoRpQSM6m45bGCJcaa6APo/embed?start=false&loop=false&delayms=60000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
       </header>
     </div>
